b5a5526b5f747dbbae4d965d7c8326f8d7f34360
Jan Tore Bøe <jan@boee.org>
2025-04-13 10:58:04 +0200

    Initial Commit

---
+++ BitbucketBuildMonitor/BitbucketBuildMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitbucketBuildMonitorApp.swift
+//  BitbucketBuildMonitor
+//
+//  Created by Jan Tore Bøe on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BitbucketBuildMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
